92d9fbd6921a1d738b03b59a9a9b6514fe6572c8
unknown <guidot@.internal.picnet.com.au>
2013-06-16 07:41:14 +1000

    Some minor refactorings

---
+++ Ml2.Tests/Kaggle/AmazonEmployee/AmazonAttributeExperiments.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Ml2.Tests.Kaggle.AmazonEmployee.Data;
+using NUnit.Framework;
+
+namespace Ml2.Tests.Kaggle.AmazonEmployee
+{
+  [TestFixture] public class AmazonAttributeExperiments
+  {
+    [Test] public void attribute_selection()
+    {
+      var train = new Runtime<AmazonTrainDataRow>(0, @"resources\kaggle\amazon-employee\train.csv");
+      var eval = train.AttributeSelection.Evaluators.CfsSubset().
+          LocallyPredictive(true).
+          MissingSeparate(true);
+      var impl = train.AttributeSelection.Algorithms.BestFirst();
+
+      var filter = train.Filters.Supervised.Attribute.AttributeSelection().
+          Evaluator(eval).
+          Search(impl);
+
+      var newrt = filter.RunFilter();
+      Console.WriteLine("New Instances: " + newrt.Instances);
+    }
+  }
+}
--- Ml2.Tests/Kaggle/Titanic/TitanicMlMiscClassifiersPredictions.cs
+++ Ml2.Tests/Kaggle/Titanic/TitanicMlMiscClassifiersPredictions.cs
@@ -1,66 +1,90 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using Ml2.Clss;
 using NUnit.Framework;
+using weka.core;
 using TR=Ml2.Tests.Kaggle.Titanic.Data.TitanicDataRow;
 
 namespace Ml2.Tests.Kaggle.Titanic
-{  
-  [TestFixture] public class TitanicMlmiscClassifiersPredictions
-  {    
+{
+  [TestFixture]
+  public class TitanicMlmiscClassifiersPredictions
+  {
     // 80.3591 %
-    [Test] public void Test_logistic_regression() {
-      var train = new Runtime<TR>(0, @"resources\kaggle\titanic\train.csv").RemoveAttributes(typeof(string));
+    [Test]
+    public void Test_logistic_regression()
+    {
+      var train = new Runtime<TR>(0, @"resources\kaggle\titanic\train.csv").RemoveAttributes(typeof (string));
 
       train.Classifiers.
-          Logistic().EvaluateWith10CrossValidation();
+        Logistic().EvaluateWith10CrossValidation();
     }
 
     // 80.3591 % ?? Even with boosting
-    [Test] public void Test_logistic_regression_with_boosting() {
-      var train = new Runtime<TR>(0, @"resources\kaggle\titanic\train.csv").RemoveAttributes(typeof(string));      
+    [Test]
+    public void Test_logistic_regression_with_boosting()
+    {
+      var train = new Runtime<TR>(0, @"resources\kaggle\titanic\train.csv").RemoveAttributes(typeof (string));
       var classifier = train.Classifiers.Logistic();
 
       train.Classifiers.
-          AdaBoostM1().Seed(1).Classifier(classifier).
-              NumIterations(10).EvaluateWith10CrossValidation();
+        AdaBoostM1().Seed(1).Classifier(classifier).
+        NumIterations(10).EvaluateWith10CrossValidation();
     }
 
-    [Test] public void Test_logistic_regression_with_and_without_ages() {
+    [Test]
+    public void Test_logistic_regression_with_and_without_ages()
+    {
       var rows = Runtime.Load<TR>(@"resources\kaggle\titanic\train.csv");
       var withage = rows.Where(t => t.Age.HasValue).ToArray();
       var without = rows.Where(t => !t.Age.HasValue).
-          Select(t => new {
-            t.Survival, t.PassengerClass, t.Sex, t.NumSiblingsOrSpouses, 
-            t.NumParentsChildren, t.PassengerFare, t.PortOfEmbarkation
-          }).
-          ToArray();
+        Select(t => new
+                      {
+                        t.Survival,
+                        t.PassengerClass,
+                        t.Sex,
+                        t.NumSiblingsOrSpouses,
+                        t.NumParentsChildren,
+                        t.PassengerFare,
+                        t.PortOfEmbarkation
+                      }).
+        ToArray();
 
       Console.WriteLine("With ages.");
       new Runtime<TR>(0, withage).
-        RemoveAttributes(typeof(string)).
-            Classifiers.Logistic().EvaluateWith10CrossValidation();
+        RemoveAttributes(typeof (string)).
+        Classifiers.Logistic().EvaluateWith10CrossValidation();
 
       Console.WriteLine("Without ages.");
       new Runtime(0, without).
-          RemoveAttributes(typeof(string)).
-          Classifiers.Logistic().EvaluateWith10CrossValidation();
+        RemoveAttributes(typeof (string)).
+        Classifiers.Logistic().EvaluateWith10CrossValidation();
     }
 
-    [Test] public void Test_creating_prediction_file()
+    [Test]
+    public void Test_creating_prediction_file()
     {
       var raw = File.ReadAllLines(@"resources\kaggle\titanic\test.csv");
       var formatted = raw.Select(l => "1," + l).ToArray();
 
       var testrows = Runtime.LoadLines<TR>(formatted);
       var testset = new Runtime<TR>(0, testrows).
-            RemoveAttributes(typeof(string));      
+        RemoveAttributes(typeof (string));
 
       var classifier = new Runtime<TR>(0, @"resources\kaggle\titanic\train.csv").
-          RemoveAttributes(typeof(string)).
-          Classifiers.Logistic();
+        RemoveAttributes(typeof (string)).
+        Classifiers.Logistic();
 
-      File.WriteAllLines("predict.csv", testset.PrependClassifications(classifier, raw));
+      var lines = raw.Select((line, idx) => GetRow(classifier, idx, testset.Observations[idx - 1].Instance, line)).ToArray();
+      File.WriteAllLines("predict.csv", lines);
+    }
+
+
+    private string GetRow(Logistic<TR> trained, int idx, Instance instance, string line)
+    {
+      var classification = idx == 0 ? "header" : trained.Classify(instance).ToString();
+      return classification + "," + line;
     }
   }
 }
